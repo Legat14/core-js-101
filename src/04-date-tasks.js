f0241c13311756353030f30666ac320c67f7cfd9
Legat <zia-d@mail.ru>
2022-07-24 19:26:21 +0400

    feat: resolve all the date tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,10 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  // throw new Error('Not implemented');
+  const date = new Date(value);
+  return date;
 }
 
 /**
@@ -34,8 +36,10 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  // throw new Error('Not implemented');
+  const date = new Date(value);
+  return date;
 }
 
 
@@ -53,8 +57,19 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  // throw new Error('Not implemented');
+  const year = date.getFullYear();
+  if (year % 4 !== 0) {
+    return false;
+  }
+  if (year % 100 !== 0) {
+    return true;
+  }
+  if (year % 400 !== 0) {
+    return false;
+  }
+  return true;
 }
 
 
@@ -73,8 +88,12 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  // throw new Error('Not implemented');
+  const timespan = endDate - startDate;
+  const tempDate = new Date(timespan);
+  const result = tempDate.toISOString().substring(11, 23);
+  return result;
 }
 
 
@@ -94,8 +113,21 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  // throw new Error('Not implemented');
+  let hours = date.getUTCHours();
+  if (hours > 12) {
+    hours -= 12;
+  }
+  const minutes = date.getUTCMinutes();
+  const hourHandAngle = (hours / 12) * 360 + 0.5 * minutes;
+  const minuteHandAngle = (minutes / 60) * 360;
+  let angleBetweenHands = Math.abs(hourHandAngle - minuteHandAngle);
+  if (angleBetweenHands > 180) {
+    angleBetweenHands = 360 - angleBetweenHands;
+  }
+  const angleInRads = (angleBetweenHands * Math.PI) / 180;
+  return angleInRads;
 }
 
 
